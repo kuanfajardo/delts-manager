1c7406108294997e8f76887b804d8698ff2a9491
Juan Diego Fajardo <jfajardo@mit.edu>
2016-06-24 20:40:12 -0400

    Initial Commit

---
+++ Delts Manager/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 6/24/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
