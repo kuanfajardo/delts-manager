61e31a9c88f2a3ed72c70a2dd09272a748e01431
Juan Diego Fajardo <jfajardo@mit.edu>
2016-06-24 22:44:22 -0400

    Create LoginViewController; Implement initial functionality

--- Delts Manager/LoginViewController.swift
+++ Delts Manager/LoginViewController.swift
@@ -10,10 +10,35 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // MARK: Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    // MARK: Actions
+    @IBAction func loginPressed() {
+        // TODO: Code for login
+        if emailTextField.text!.isEmpty || passwordTextField.text!.isEmpty {
+            //debugTextLabel.text = "Username or Password Empty."
+        } else {
+            setUIEnabled(false)
+            authenticateLogin()
+        }
+    }
+    
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
+        
+        // TODO: make status bar white
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +46,20 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: Authentication
+    private func authenticateLogin() {
+        // TODO: Later, implement actual login procedure
+        if emailTextField.text! == Constants.UserInfo.email && passwordTextField.text! == Constants.UserInfo.password {
+            completeLogin()
+        } else {
+            setUIEnabled(true)
+        }
+    }
+    
+    private func completeLogin() {
+        let controller = self.storyboard?.instantiateViewControllerWithIdentifier("TabController") as! UITabBarController
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
@@ -31,5 +70,53 @@ class LoginViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+// MARK: LoginViewController - UITextFieldDelegate and Keyboard
+extension LoginViewController: UITextFieldDelegate {
+    
+    // MARK: Text Field Delegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        switch textField.accessibilityIdentifier! {
+            case "Email Field":
+                textField.resignFirstResponder()
+                passwordTextField.becomeFirstResponder()
+            case "Password Field":
+                textField.resignFirstResponder()
+                loginPressed()
+            default:
+                break
+        }
+        return true
+    }
+    
+    // MARK: Keyboard
+    private func resignIfFirstResponder(textField: UITextField) {
+        if textField.isFirstResponder() {
+            textField.resignFirstResponder()
+        }
+    }
+    
+    @IBAction func userDidTapView(sender: AnyObject) {
+        resignIfFirstResponder(emailTextField)
+        resignIfFirstResponder(passwordTextField)
+    }
+}
+
+
+// MARK: LoginViewController - Configure UI
+extension LoginViewController {
+    
+    func setUIEnabled(enabled: Bool) {
+        emailTextField.enabled = enabled
+        passwordTextField.enabled = enabled
+        loginButton.enabled = enabled
+        
+        // adjust login button opacity
+        if enabled {
+            loginButton.alpha = 1.0
+        } else {
+            loginButton.alpha = 0.5
+        }
+    }
 }
