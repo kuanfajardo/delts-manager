497ff731c42116a4cc984f4c1650f55445def561
Juan Diego Fajardo <jfajardo@mit.edu>
2016-06-24 22:33:36 -0400

    Create Constants file

---
+++ Delts Manager/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 6/24/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import UIKit
+
+// MARK: Constants
+
+struct Constants {
+    // MARK: User Info
+    struct UserInfo {
+        static let email = "jfajardo@mit.edu"
+        static let password = "yeet"
+    }
+}
