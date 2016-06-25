76519f34e956a0f208a1b96849d01db60010fa55
Juan Diego Fajardo <jfajardo@mit.edu>
2016-06-24 23:52:38 -0400

    Create DutiesTVC and Cell Files

---
+++ Delts Manager/DutiesTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DutiesTableViewController.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 6/24/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import Cocoa
+
+class DutiesTableViewController: UITableViewController {
+
+}
