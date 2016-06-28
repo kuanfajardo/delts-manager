bec43a4757cd79d2a57a25e9c8de1ed920eda9fb
Juan Diego Fajardo <jfajardo@mit.edu>
2016-06-28 17:12:36 +0200

    Combine deleted files into new file for better model

---
+++ Delts Manager/DutiesViewController.swift
@@ -0,0 +1,59 @@
+//
+//  DutiesDataSourceDelegate.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 6/28/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import UIKit
+
+class DutiesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    // MARK: Outlets
+    @IBOutlet weak var tableview: UITableView!
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableview.delegate = self
+        tableview.dataSource = self
+    }
+    
+    // MARK: Properties:
+    var duties = [Duty]()
+    
+    func loadSampleDuties() {
+        let duty1 = Duty(name: "Pantry", date: NSDate(), status: "Completed")
+        let duty2 = Duty(name: "Pantry", date: NSDate(), status: "Incomplete")
+        let duty3 = Duty(name: "Kitchen", date: NSDate(), status: "Pending")
+        
+        duties += [duty1, duty2, duty3]
+    }
+
+    
+    // MARK: UITableViewDataSource
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        print("yeeet")
+        let identifier = "DutyTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath) as! DutiesTableViewCell
+        
+        let duty = duties[indexPath.row]
+        
+        cell.dutyLabel.text = duty.name
+        cell.statusLabel.text = duty.status
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .ShortStyle
+        cell.dateLabel.text = dateFormatter.stringFromDate(duty.date)
+        print(cell)
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return duties.count
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+}
