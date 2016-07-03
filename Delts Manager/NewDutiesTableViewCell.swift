0c30d7d3f584d26ab6fb3507ac8a8e06f313a461
Juan Diego Fajardo <jfajardo@mit.edu>
2016-07-03 23:30:47 +0200

    Add files for new duties section

---
+++ Delts Manager/NewDutiesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewDutiesTableViewCell.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 7/3/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import UIKit
+
+class NewDutiesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
