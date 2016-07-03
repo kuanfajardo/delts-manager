04d1a707e8bc0dcbf45ec473432a21edecb5e211
Juan Diego Fajardo <jfajardo@mit.edu>
2016-07-03 21:49:32 +0200

    Create class for setting switch table cell

---
+++ Delts Manager/SettingsSwitchTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsSwitchTableViewCell.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 6/30/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsSwitchTableViewCell: UITableViewCell {
+
+}
