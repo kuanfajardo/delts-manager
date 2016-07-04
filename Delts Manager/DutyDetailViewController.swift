92146a06a8b004f337f9a32d146d2ee0fb392010
Juan Diego Fajardo <jfajardo@mit.edu>
2016-07-04 09:23:01 +0200

    Add files for new Detail Controller

---
+++ Delts Manager/DutyDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DutyDetailViewController.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 7/4/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import Cocoa
+
+class DutyDetailViewController: UIViewController {
+
+}
