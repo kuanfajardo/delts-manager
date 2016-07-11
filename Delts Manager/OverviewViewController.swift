b24ee218ff71456d044968e4c4cd7fb6416e4191
Juan Diego Fajardo <jfajardo@mit.edu>
2016-07-11 13:17:09 +0200

    Add custom tab bar icon files; Add new file for first overview class

---
+++ Delts Manager/OverviewViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OverviewViewController.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 7/5/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import Cocoa
+
+class OverviewViewController: UIViewController {
+
+}
