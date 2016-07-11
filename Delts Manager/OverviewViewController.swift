15c8a08c4a572e9ead010ed49aa211bf42f7f4dc
Juan Diego Fajardo <jfajardo@mit.edu>
2016-07-11 13:36:31 +0200

    First rendition of overview page // with UI and class

--- Delts Manager/OverviewViewController.swift
+++ Delts Manager/OverviewViewController.swift
@@ -6,8 +6,68 @@
 //  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class OverviewViewController: UIViewController {
-
+    // MARK: Outlets
+    @IBOutlet weak var checkoffImageView: UIImageView!
+    @IBOutlet weak var puntMakeupImageView: UIImageView!
+    @IBOutlet weak var scheduleImageView: UIImageView!
+    @IBOutlet weak var dutyLabel: UILabel!
+    @IBOutlet weak var puntLabel: UILabel!
+    @IBOutlet weak var scheduleLabel: UILabel!
+    
+    // MARK: Actions
+    @IBAction func checkoffPressed() {
+        //
+    }
+    
+    @IBAction func puntMakeupPressed() {
+        //
+    }
+    
+    @IBAction func schedulePressed() {
+        //
+    }
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+    
+    
+    func setUI() {
+        let checkoffEnabled = Constants.defaults.boolForKey(Constants.DefaultsKeys.CheckoffEnabled)
+        let numPunts = Constants.defaults.integerForKey(Constants.DefaultsKeys.Punts)
+        let scheduleEnabled = Constants.defaults.boolForKey(Constants.DefaultsKeys.ScheduleEnabled)
+        
+        // Duty Section
+        self.checkoffImageView.userInteractionEnabled = checkoffEnabled
+        
+        if checkoffEnabled {
+            //self.dutyLabel.text =
+        } else {
+            self.dutyLabel.text = "No Duties Today :)"
+        }
+        
+        // Punts Section
+        self.puntLabel.text = "\(numPunts)"
+        
+        if numPunts > 0 {
+            self.puntMakeupImageView.userInteractionEnabled = true
+        } else {
+            self.puntMakeupImageView.userInteractionEnabled = true
+        }
+        
+        // Schedule Section
+        self.scheduleLabel.userInteractionEnabled = scheduleEnabled
+        
+        if scheduleEnabled {
+            self.scheduleLabel.text = "Open"
+        } else {
+            self.scheduleLabel.text = "Closed: Friday 5:00"
+        }
+    }
+    
 }
