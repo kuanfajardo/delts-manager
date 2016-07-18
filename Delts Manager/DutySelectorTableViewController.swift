e63b053369e170b737bd39250a69601fa9c4aef4
Juan Diego Fajardo <jfajardo@mit.edu>
2016-07-18 09:37:03 +0200

    Add new file for duty selector

---
+++ Delts Manager/DutySelectorTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DutySelectorTableViewController.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 7/18/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import Cocoa
+
+class DutySelectorTableViewController: UITableViewController {
+
+}
