b8bd1154eda937cd89a13c8059ec2749fd6eca36
Juan Diego Fajardo <jfajardo@mit.edu>
2016-07-19 17:29:52 +0200

    Add new files for Bouncing Tab and Event structure

---
+++ Delts Manager/BouncingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BouncingViewController.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 7/19/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import Cocoa
+
+class BouncingViewController: UIViewController {
+
+}
---
+++ Delts Manager/Event.swift
@@ -0,0 +1,9 @@
+//
+//  Event.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 7/19/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import Foundation
