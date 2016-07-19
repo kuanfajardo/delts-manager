b2f50b5f303bda6992e3b9b609c97c7b7d179dee
Juan Diego Fajardo <jfajardo@mit.edu>
2016-07-19 17:57:26 +0200

    Early implementation of Event

--- Delts Manager/Event.swift
+++ Delts Manager/Event.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Event {
+    // MARK: Properties
+    var name: String
+    var startTime: NSDate
+    var endTime: NSDate
+    var duties: [Duty]
+    var people: [String]
+    
+    init(name: String, startTime: NSDate, endTime: NSDate, duties: [Duty], people: [String]) {
+        self.name = name
+        self.startTime = startTime
+        self.endTime = endTime
+        self.duties = duties
+        self.people = people
+    }
+}
