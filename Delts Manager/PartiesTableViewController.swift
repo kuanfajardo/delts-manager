a9652e366dd4f4610784061f2d4eb0d1850be52f
Juan Diego Fajardo <jfajardo@mit.edu>
2016-07-25 16:54:44 +0200

    Add files for party tab

---
+++ Delts Manager/PartiesTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PartiesTableViewController.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 7/25/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import Cocoa
+
+class PartiesTableViewController: UITableViewController {
+
+}
---
+++ Delts Manager/PartyTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PartyTableViewCell.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 7/25/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import Cocoa
+
+class PartyTableViewCell: UITableViewCell {
+
+}
