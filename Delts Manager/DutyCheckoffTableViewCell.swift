23717a5bd90bf24e488f5ecbfd4d04af04131c74
Juan Diego Fajardo <jfajardo@mit.edu>
2016-07-29 13:15:42 +0200

    Add files for Duty Checkoff cell

---
+++ Delts Manager/DutyCheckoffTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DutyCheckoffTableViewCell.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 7/29/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import Cocoa
+
+class DutyCheckoffTableViewCell: UITableViewCell {
+
+}
