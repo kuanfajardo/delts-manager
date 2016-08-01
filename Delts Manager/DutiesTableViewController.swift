648d7955494cb859be7087eed2a4a0ec86b0ead1
Juan Diego Fajardo <jfajardo@mit.edu>
2016-08-01 13:18:32 +0200

    implement diff cells for diff segments in duties

--- Delts Manager/DutiesTableViewController.swift
+++ Delts Manager/DutiesTableViewController.swift
@@ -16,18 +16,18 @@ class DutiesTableViewController: UITableViewController {
         
         // Check roles for user content
         if Constants.userAuthorized(Constants.Roles.Checker) || Constants.userAuthorized(Constants.Roles.HouseManager) || Constants.userAuthorized(Constants.Roles.Admin) {
-            let segControl = UISegmentedControl(items: ["User", "Checker"])
+            self.segControl = UISegmentedControl(items: ["User", "Checker"])
             
             if Constants.userAuthorized(Constants.Roles.HouseManager) || Constants.userAuthorized(Constants.Roles.Admin) {
-                segControl.insertSegmentWithTitle("Admin", atIndex: 2, animated: false)
+                self.segControl!.insertSegmentWithTitle("Admin", atIndex: 2, animated: false)
             }
             
-            segControl.addTarget(self, action: #selector(segmentChanged), forControlEvents: .ValueChanged)
-            segControl.tintColor = Constants.Colors.deltsDarkPurple
-            segControl.selectedSegmentIndex = 0
+            self.segControl!.addTarget(self, action: #selector(segmentChanged), forControlEvents: .ValueChanged)
+            self.segControl!.tintColor = Constants.Colors.deltsDarkPurple
+            self.segControl!.selectedSegmentIndex = 0
 
             
-            self.navigationItem.titleView = segControl
+            self.navigationItem.titleView = self.segControl
         }
         
         // loadDuties()
@@ -35,52 +35,99 @@ class DutiesTableViewController: UITableViewController {
     }
     
     func segmentChanged(sender: UISegmentedControl) {
-        print(sender.selectedSegmentIndex)
+        self.tableView.reloadData()
     }
     
     // MARK: Properties:
     var duties = [Duty]()
     
     func loadSampleDuties() {
-        let duty1 = Duty(name: "Pantry", type: Constants.DutyType.House, date: NSDate(), status: "Completed")
-        let duty2 = Duty(name: "Pantry", type: Constants.DutyType.House, date: NSDate(), status: "Incomplete")
-        let duty3 = Duty(name: "Kitchen", type: Constants.DutyType.House, date: NSDate(), status: "Pending")
+        let duty1 = Duty(slave: "Juan", name: "Pantry", type: Constants.DutyType.House, date: NSDate(), status: "Completed")
+        let duty2 = Duty(slave: "Juan", name: "Pantry", type: Constants.DutyType.House, date: NSDate(), status: "Incomplete")
+        let duty3 = Duty(slave: "Juan", name: "Kitchen", type: Constants.DutyType.House, date: NSDate(), status: "Pending")
         
         duties += [duty1, duty2, duty3]
+    }
+    
+    var segControl: UISegmentedControl?
+    var segment: String {
+        if self.segControl == nil {
+            return "User"
+        } else {
+            return (self.segControl!.titleForSegmentAtIndex((self.segControl?.selectedSegmentIndex)!))!
+        }
     }
     
     
     // MARK: UITableViewDataSource
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
-        let identifier = Constants.Identifiers.TableViewCells.DutyTableViewCell
-        let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath) as! DutiesTableViewCell
-        
-        if indexPath.row % 2 == 0 {
-            cell.backgroundColor = Constants.Colors.deltsLightPurple
-        } else {
-            cell.backgroundColor = Constants.Colors.deltsYellow
-        }
         
         guard indexPath.row < duties.count else {
-            cell.dutyLabel.text = ""
-            cell.dateLabel.text = ""
-            cell.statusImageView?.image = nil
+            let identifier = Constants.Identifiers.TableViewCells.PlainCell
+            let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath)
+            
+            if indexPath.row % 2 == 0 {
+                cell.backgroundColor = Constants.Colors.deltsLightPurple
+            } else {
+                cell.backgroundColor = Constants.Colors.deltsYellow
+            }
+            
             cell.userInteractionEnabled = false
             
             return cell
         }
         
-        let duty = duties[indexPath.row]
-        
-        cell.duty = duty
-        cell.dutyLabel.text = duty.name
-        cell.dateLabel.text = duty.dateString
-        // TODO: Real
-        cell.statusImageView?.image = UIImage(named: Constants.Photos.BlackCircle)
-        cell.selectionStyle = .Gray
-        
-        return cell
+        if self.segment == "Checker" || self.segment == "Admin" {
+            let identifier = Constants.Identifiers.TableViewCells.DutyCheckoffCell
+            let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath) as! DutyCheckoffTableViewCell
+
+            if indexPath.row % 2 == 0 {
+                cell.backgroundColor = Constants.Colors.deltsLightPurple
+            } else {
+                cell.backgroundColor = Constants.Colors.deltsYellow
+            }
+
+            let duty = duties[indexPath.row]
+            cell.duty = duty
+            cell.dutyLabel.text = duty.name
+            cell.dateLabel.text = duty.dateString
+            cell.slaveLabel.text = duty.slave
+            // TODO: Real
+            cell.statusImageView?.image = UIImage(named: Constants.Photos.BlackCircle)
+            cell.selectionStyle = .Gray
+            return cell
+
+        } else {
+            let identifier = Constants.Identifiers.TableViewCells.DutyTableViewCell
+            let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath) as! DutiesTableViewCell
+            
+            if indexPath.row % 2 == 0 {
+                cell.backgroundColor = Constants.Colors.deltsLightPurple
+            } else {
+                cell.backgroundColor = Constants.Colors.deltsYellow
+            }
+            
+            guard indexPath.row < duties.count else {
+                cell.dutyLabel.text = ""
+                cell.dateLabel.text = ""
+                cell.statusImageView?.image = nil
+                cell.userInteractionEnabled = false
+                
+                return cell
+            }
+            
+            let duty = duties[indexPath.row]
+            
+            cell.duty = duty
+            cell.dutyLabel.text = duty.name
+            cell.dateLabel.text = duty.dateString
+            // TODO: Real
+            cell.statusImageView?.image = UIImage(named: Constants.Photos.BlackCircle)
+            cell.selectionStyle = .Gray
+            
+            return cell
+        }
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -91,7 +138,9 @@ class DutiesTableViewController: UITableViewController {
         return 1
     }
 
-
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
     
     // MARK: - Navigation
 
