1bf386cffcf3ca3a95c494727469d6d4bf7bc300
Juan Diego Fajardo <jfajardo@mit.edu>
2016-08-01 14:56:46 +0200

    Add files for punt admin cell

---
+++ Delts Manager/PuntAdminTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PuntAdminTableViewCell.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 8/1/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import Cocoa
+
+class PuntAdminTableViewCell: UITableViewCell {
+
+}
