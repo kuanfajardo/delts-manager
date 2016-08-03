1e0622a48802b0056b99c7d2021c44f6dbed90bc
Juan Diego Fajardo <jfajardo@mit.edu>
2016-08-03 18:56:47 +0200

    Populate Invite class

--- Delts Manager/Invite.swift
+++ Delts Manager/Invite.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+class Invite {
+    // MARK: Properties
+    var inviteName: String?
+    var deltName: String?
+    
+    // MARK: Life Cycle
+    convenience init(invite: String) {
+        self.init(invite: invite, from: Constants.defaults.stringForKey(Constants.DefaultsKeys.Name)!)
+    }
+    
+    init(invite: String, from: String) {
+        self.inviteName = invite
+        self.deltName = from
+    }
+}
