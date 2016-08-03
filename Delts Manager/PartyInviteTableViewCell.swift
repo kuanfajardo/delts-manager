f1301e17873a8add60008bd3bdd925c1c3b58828
Juan Diego Fajardo <jfajardo@mit.edu>
2016-08-03 18:47:34 +0200

    New files for party invite cell

---
+++ Delts Manager/PartyInviteTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PartyInviteTableViewCell.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 8/3/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import Cocoa
+
+class PartyInviteTableViewCell: UITableViewCell {
+
+}
