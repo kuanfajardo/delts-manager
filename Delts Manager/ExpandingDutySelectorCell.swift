5ba4d447c899e63337ef66338c679102376e4221
Juan Diego Fajardo <jfajardo@mit.edu>
2016-08-06 15:45:45 +0200

    Populate new cell class

--- Delts Manager/ExpandingDutySelectorCell.swift
+++ Delts Manager/ExpandingDutySelectorCell.swift
@@ -6,8 +6,20 @@
 //  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
 //
 
-import Cocoa
+import UIKit
+import ExpandingTableView
 
-class ExpandingDutySelectorCell: UITableViewCell {
+class ExpandingDutySelectorCell: ExpandingTableViewCell {
+    // MARK: Properties
+    var duty: Duty?
+    //var reuseId = "ExpandingDutySelector"
+    
+    // MARK: Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var mondayButton: UIButton!
+    @IBOutlet weak var tuesdayButton: UIButton!
+    @IBOutlet weak var wednesdayButton: UIButton!
+    @IBOutlet weak var thursdayButton: UIButton!
+    @IBOutlet weak var fridayButton: UIButton!
 
 }
