eca08fb65bf497cbd08e440893511cda2fc3431a
Juan Diego Fajardo <jfajardo@mit.edu>
2016-08-07 21:34:15 -0400

    Pod install: Chameleon

--- Pods/Target Support Files/Pods-Delts Manager/Pods-Delts Manager-frameworks.sh
+++ Pods/Target Support Files/Pods-Delts Manager/Pods-Delts Manager-frameworks.sh
@@ -85,6 +85,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ChameleonFramework/ChameleonFramework.framework"
   install_framework "$BUILT_PRODUCTS_DIR/DatePickerCell/DatePickerCell.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ExpandingTableView/ExpandingTableView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Freddy/Freddy.framework"
@@ -92,6 +93,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ChameleonFramework/ChameleonFramework.framework"
   install_framework "$BUILT_PRODUCTS_DIR/DatePickerCell/DatePickerCell.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ExpandingTableView/ExpandingTableView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Freddy/Freddy.framework"
