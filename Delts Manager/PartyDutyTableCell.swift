dfa6b7c5716d803c217b71c27d387711b2fefe48
Juan Diego Fajardo <jfajardo@mit.edu>
2016-08-10 01:10:13 -0400

    Add file: partydutycell

---
+++ Delts Manager/PartyDutyTableCell.swift
@@ -0,0 +1,13 @@
+//
+//  PartyDutyTableCell.swift
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 8/10/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+import Cocoa
+
+class PartyDutyTableCell: UITableViewCell {
+
+}
