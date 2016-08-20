4e393d175a2b1ff19d299d303824241cd3a168f4
Juan Diego Fajardo <jfajardo@mit.edu>
2016-08-20 13:13:32 -0400

    Add bridging header file to project

---
+++ Delts Manager/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  Delts Manager
+//
+//  Created by Juan Diego Fajardo on 8/20/16.
+//  Copyright © 2016 Juan Diego Fajardo. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
