4ffd32cbdf74187c9574488cb4ee33984e989d65
Juan Diego Fajardo <jfajardo@mit.edu>
2016-08-22 01:10:52 -0400

    Add like a ton of convenience inits to Duty

--- Delts Manager/Duty.swift
+++ Delts Manager/Duty.swift
@@ -40,6 +40,72 @@ class Duty {
     
     
     // MARK: Init
+    convenience init(json: JSON) throws {
+        let dutyID = try json.int("duty_id")
+        let dutyDateString = try json.string("date")
+        let dutyName = try json.string("duty_name")
+        let dutyDescription = try json.string("description")
+        let dutyStatusString = try json.string("status")
+        let checkerName = try json.string("checker")
+        let checkComments = try json.string("check_comments")
+        
+        self.init(slave: Constants.defaults.stringForKey(Constants.DefaultsKeys.Name)!, name: dutyName, id: dutyID, description: dutyDescription, checker: checkerName, comments: checkComments, status: dutyStatusString, date: dutyDateString)
+    }
+    
+    
+    // LAZY CONVENIENCE INIT
+    convenience init() {
+        self.init(slave: "Delt", name: "Duty", id: 0, description: "", checker: "N/A", comments: "")
+    }
+    
+    // MAIN CONVENIENCE INIT
+    convenience init(slave: String, name: String, id: Int, description: String, checker: String, comments: String, status: String, date: String) {
+        
+        // Calculating Status
+        let realStatus: DutyStatus
+        
+        switch status {
+        case "Complete":
+            realStatus = .Complete
+            break
+        case "Pending":
+            realStatus = .Pending
+            break
+        case "Incomplete":
+            realStatus = .Incomplete
+            break
+        case "Punted":
+            realStatus = .Punted
+            break
+        default:
+            realStatus = .Incomplete
+        }
+        
+        // Calculating Date
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let realDate = dateFormatter.dateFromString(date)!
+        
+        // Call other init
+        self.init(slave: slave, name: name, id: id, description: description, checker: checker, comments: comments, status: realStatus, date: realDate)
+        
+    }
+    
+    // MAIN INIT
+    init(slave: String, name: String, id: Int, description: String, checker: String, comments: String, type: Constants.DutyType = Constants.DutyType.House, status: DutyStatus = DutyStatus.Incomplete, date: NSDate = NSDate()) {
+        self.slave = slave
+        self.name = name
+        self.id = id
+        self.description = description
+        self.checker = checker
+        self.checkComments = comments
+        self.type = type
+        self.status = status
+        self.date = date
+    }
+    
+    
+    // OLD INITS
     // House
     init(slave: String, name: String, id: Int, type: Constants.DutyType, date: NSDate, status: DutyStatus) {
         self.slave = slave
