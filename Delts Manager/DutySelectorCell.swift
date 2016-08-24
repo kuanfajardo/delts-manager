34335b7780f7a2b5216f04bfefef140a1c95e47d
Juan Diego Fajardo <jfajardo@mit.edu>
2016-08-24 13:08:07 -0400

    Implement house duty selection! And deselection

--- Delts Manager/DutySelectorCell.swift
+++ Delts Manager/DutySelectorCell.swift
@@ -15,6 +15,7 @@ class DutySelectorCell: UITableViewCell {
     var duty: Duty?
     var houseDuty: HouseDuty?
     //var enabled = [Bool]()
+    var delegate: UIViewController?
     
     // MARK: Outlets
     @IBOutlet weak var titleLabel: UILabel!
@@ -27,50 +28,100 @@ class DutySelectorCell: UITableViewCell {
     // MARK: Actions
     // IB
     @IBAction func mondayButtonPressed() {
-        print("monday")
-        print(mondayButton.tag)
-        updateHouseDuties()
+        buttonPressedForDay(1)
     }
     
     @IBAction func tuesdayButtonPressed() {
-        print("tuesday")
-        print(tuesdayButton.tag)
-        updateHouseDuties()
+        buttonPressedForDay(2)
     }
     
     @IBAction func wednesdayButtonPressed() {
-        print("wednesday")
-        print(wednesdayButton.tag)
-        updateHouseDuties()
+        buttonPressedForDay(3)
     }
     
     @IBAction func thursdayButtonPressed() {
-        print("thursday")
-        print(thursdayButton.tag)
-        updateHouseDuties()
+        buttonPressedForDay(4)
     }
     
     @IBAction func fridayButtonPressed() {
-        print("friday")
-        print(fridayButton.tag)
-        updateHouseDuties()
+        buttonPressedForDay(5)
+    }
+    
+    // MARK: main action
+    func buttonPressedForDay(dayOfWeek: Int) {
+        let availability = self.houseDuty!.days[dayOfWeek]
+        let claim: Bool?
+        var alertMessage = ""
+        
+        print(self.houseDuty?.name)
+        print(availability)
+        
+        switch availability {
+        case .Open:
+            claim = true
+            break
+        case .Selected:
+            claim = false
+            break
+        case .Taken:
+            claim = nil
+            alertMessage = "This duty is taken by \(self.houseDuty!.takers[dayOfWeek])"
+            break
+        case .Unavailable:
+            claim = nil
+            alertMessage = "This duty is grey. Grey = Unavailable. How are you at MIT."
+        }
+        
+        print(claim)
+        print(alertMessage)
+        
+        guard claim != nil else {
+            let alertController = UIAlertController(title: "Nope. Stop.", message: alertMessage, preferredStyle: .Alert)
+            
+            let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            alertController.addAction(okAction)
+            
+            self.delegate?.presentViewController(alertController, animated: true, completion: nil)
+            
+            return
+        }
+        
+        let dutyID = self.houseDuty!.ids[dayOfWeek]
+        
+        updateHouseDuty(withID: dutyID, forClaim: claim!)
     }
     
     
-    // TODO: how to implement - update all duties at same time? or one at a time when pressed? when pressed done? or on duty? reflect with constant as well
-    func updateHouseDuties() {
+
+    func updateHouseDuty(withID dutyID: Int, forClaim claim: Bool) {
         let methodParameters = [
             Constants.AlamoKeys.ApiKey : Constants.AlamoValues.ApiKey,
-            Constants.AlamoKeys.Token : Constants.AlamoValues.Token
-        ]
+            Constants.AlamoKeys.Token : Constants.AlamoValues.Token,
+            Constants.AlamoKeys.DutyID : dutyID,
+            Constants.AlamoKeys.Claim : claim
+        ] as! [String : AnyObject]
         
-        print("Request to \(DeltURLWithMethod(Constants.Networking.Methods.UpdateHouseDuty))")
+        print("Request to \(DeltURLWithMethod(Constants.Networking.Methods.UpdateHouseDuty)) for \(dutyID) claim \(claim)")
         Alamofire.request(.POST, DeltURLWithMethod(Constants.Networking.Methods.UpdateHouseDuty), parameters: methodParameters)
             .validate(contentType: ["application/json"])
             .responseJSON { (response) in
                 do {
                     let json = try JSON(data: response.data!)
-                    // Rest of parsing here
+                    
+                    let statusCode = try json.int("status")
+                    
+                    let alertTitle = statusCode == 1 ? "Success!" : "Fail"
+                    let alertMessage = statusCode == 1 ? "Duty \(claim == true ? "claimed" : "disclaimed") successfully" : "Something went wrong."
+                    
+                    let alertController = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .Alert)
+                    
+                    let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                    alertController.addAction(okAction)
+                    
+                    self.delegate?.presentViewController(alertController, animated: true, completion: nil)
+                    
+                    return
+                    
                 } catch {
                     print("Error in updateHouseDuties")
                 }
--- Delts Manager/DutySelectorTableViewController.swift
+++ Delts Manager/DutySelectorTableViewController.swift
@@ -18,7 +18,7 @@ class DutySelectorTableViewController: UITableViewController {
         super.viewDidLoad()
 
         // Navigation items
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Cancel, target: self, action: #selector(cancelPressed))
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Refresh", style: .Plain, target: self, action: #selector(refreshPressed))
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: #selector(donePressed))
         
         self.navigationItem.leftBarButtonItem?.tintColor = UIColor.flatWhiteColor()
@@ -29,48 +29,37 @@ class DutySelectorTableViewController: UITableViewController {
         self.tableView.estimatedRowHeight = 88
         self.tableView.bounces = false
 
-        loadSampleDuties()
         loadSampleDutyNames()
         loadSampleHouseDuties()
         
         loadHouseDuties()
     }
     
-    func cancelPressed() {
-        // TODO: Add functionality
-        self.navigationController?.popViewControllerAnimated(true)
+    func refreshPressed() {
+        loadHouseDuties()
+        self.tableView.reloadData()
     }
     
     func donePressed() {
-        // TODO: Add functionality
         self.navigationController?.popViewControllerAnimated(true)
     }
     
     // MARK: Properties:
-    var duties = [Duty]()
     var dutyNames = [String]()
     var houseDuties = [HouseDuty]()
     
     // MARK: Helper Functions
-    func loadSampleDuties() {
-        let duty1 = Duty(slave: "", name: "Pantry", id: 0, type: Constants.DutyType.House, date: NSDate(), status: .Complete)
-        let duty2 = Duty(slave: "", name: "Pantry", id: 0, type: Constants.DutyType.House, date: NSDate(), status: .Pending)
-        let duty3 = Duty(slave: "", name: "Kitchen", id: 0, type: Constants.DutyType.House, date: NSDate(), status: .Punted)
-        
-        duties += [duty1, duty2, duty3]
-    }
-    
     func loadSampleDutyNames() {
         self.dutyNames = ["Pantry One", "Pantry Two", "Kitchen", "Basement", "1st / Foyer", "2nd Big / Trash", "2nd Little / Vacuum", "3rd Big / Trash", "3rd Little / Vacuum", "4th Big / Trash", "4th Little/ Vacuum"]
     }
     
     func loadSampleHouseDuties() {
-        let duty1 = HouseDuty(name: "Pantry One", days: [.Open,.Open,.Taken,.Taken,.Open])
-        let duty2 = HouseDuty(name: "Pantry Two", days: [.Taken,.Open,.Taken,.Taken,.Open])
-        let duty3 = HouseDuty(name: "Kitchen", days: [.Open,.Unavailable,.Taken,.Unavailable,.Open])
-        let duty4 = HouseDuty(name: "Basement", days: [.Taken,.Unavailable,.Taken,.Unavailable,.Taken])
-        let duty5 = HouseDuty(name: "1st / Foyer", days: [.Open,.Unavailable,.Open,.Unavailable,.Open])
-        let duty6 = HouseDuty(name: "2nd Little / Vacuum", days: [.Taken,.Unavailable,.Open,.Unavailable,.Open])
+        let duty1 = HouseDuty(name: "Pantry One", days: [.Unavailable, .Selected,.Open,.Taken,.Taken,.Open, .Unavailable])
+        let duty2 = HouseDuty(name: "Pantry Two", days: [.Unavailable, .Taken,.Open,.Taken,.Taken,.Open, .Unavailable])
+        let duty3 = HouseDuty(name: "Kitchen", days: [.Unavailable, .Open,.Unavailable,.Taken,.Unavailable,.Open, .Unavailable])
+        let duty4 = HouseDuty(name: "Basement", days: [.Unavailable, .Taken,.Unavailable,.Taken,.Unavailable,.Taken, .Unavailable])
+        let duty5 = HouseDuty(name: "1st / Foyer", days: [.Unavailable, .Open,.Unavailable,.Open,.Unavailable,.Open, .Unavailable])
+        let duty6 = HouseDuty(name: "2nd Little / Vacuum", days: [.Unavailable, .Taken,.Unavailable,.Open,.Unavailable,.Open, .Unavailable])
         
         self.houseDuties += [duty1, duty2, duty3, duty4, duty5, duty6]
     }
@@ -93,21 +82,10 @@ class DutySelectorTableViewController: UITableViewController {
             return cell
         }
         
-        //let cell = super.tableView(tableView, cellForRowAtIndexPath: indexPath) as! ExpandingDutySelectorCell
         let identifier = Constants.Identifiers.TableViewCells.NewDutySelectorCell
         let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath) as! DutySelectorCell
-        /*
-        if indexPath.row % 2 == 1 {
-            cell.backgroundColor = Constants.Colors.deltsYellow//UIColor.flatMagentaColor()//Constants.Colors.deltsPurple
-        } else {
-            cell.backgroundColor = Constants.Colors.deltsYellow
-        }*/
-        
-        /*let duty = duties[indexPath.row]
-        
-        cell.duty = duty
-        cell.titleLabel.text = duty.name*/
-        
+        cell.delegate = self
+                
         let houseDuty = houseDuties[indexPath.row]
         cell.houseDuty = houseDuty
         cell.titleLabel.text = houseDuty.name
@@ -118,11 +96,12 @@ class DutySelectorTableViewController: UITableViewController {
         cell.thursdayButton.tag = indexPath.row
         cell.fridayButton.tag = indexPath.row
         
-        cell.mondayButton.backgroundColor = colorFromAvailability(houseDuty.days[0])
-        cell.tuesdayButton.backgroundColor = colorFromAvailability(houseDuty.days[1])
-        cell.wednesdayButton.backgroundColor = colorFromAvailability(houseDuty.days[2])
-        cell.thursdayButton.backgroundColor = colorFromAvailability(houseDuty.days[3])
-        cell.fridayButton.backgroundColor = colorFromAvailability(houseDuty.days[4])
+        cell.mondayButton.backgroundColor = colorFromAvailability(houseDuty.days[1])
+        cell.tuesdayButton.backgroundColor = colorFromAvailability(houseDuty.days[2])
+        cell.wednesdayButton.backgroundColor = colorFromAvailability(houseDuty.days[3])
+        cell.thursdayButton.backgroundColor = colorFromAvailability(houseDuty.days[4])
+        cell.fridayButton.backgroundColor = colorFromAvailability(houseDuty.days[5])
+        
         
         cell.selectionStyle = .None
         
@@ -150,7 +129,27 @@ class DutySelectorTableViewController: UITableViewController {
             .responseJSON { (response) in
                 do {
                     let json = try JSON(data: response.data!)
-                    // Rest of parsing here
+                    
+                    let dutiesJSON = try json.array(0)
+                    
+                    for duty in dutiesJSON {
+                        let dutyID = try duty.int("duty_id")
+                        let dayOfWeek = try duty.int("day_of_week")
+                        let userName = try duty.string("user_name")
+                        let dutyName = try duty.string("duty_name")
+                        
+                        let availability = self.availabilityFromName(userName)
+                        
+                        if self.dutyNames.indexOf(dutyName) == nil {
+                            self.dutyNames.append(dutyName)
+                            let newHouseDuty = HouseDuty(name: dutyName)
+                            self.houseDuties.append(newHouseDuty)
+                        }
+                        
+                        self.houseDuties[self.dutyNames.indexOf(dutyName)!].updateForDay(dayOfWeek, withAvailability: availability, takenBy: userName, dutyID: dutyID)
+                        
+                        
+                    }
                 } catch {
                     print("Error in loadHouseDuties")
                 }
@@ -169,6 +168,17 @@ class DutySelectorTableViewController: UITableViewController {
             return UIColor.flatWatermelonColor()
         case .Unavailable:
             return UIColor.flatGrayColorDark()
+        }
+    }
+    
+    func availabilityFromName(userName: String) -> DutyAvailability{
+        switch userName {
+        case "Available":
+            return DutyAvailability.Open
+        case Constants.defaults.stringForKey(Constants.DefaultsKeys.Name)!:
+            return DutyAvailability.Selected
+        default:
+            return DutyAvailability.Taken
         }
     }
     
